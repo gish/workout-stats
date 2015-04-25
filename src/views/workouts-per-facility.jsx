fa913ebecfa6eca9f9d67311a9d98410f2f0f402
Erik Hedberg <erik@gish.se>
2015-04-25 11:43:28 +0200

    Add dropdown to sort facilities

--- src/views/workouts-per-facility.jsx
+++ src/views/workouts-per-facility.jsx
@@ -31,20 +31,54 @@ Main = React.createClass({
         return visitedFacilities;
     },
 
+    getFacilitiesSortedBy: function(key) {
+        var facilities = this.getVisitedFacilities(this.props.log),
+            sortMethods = {
+                visits: function(a, b) {
+                    return b[key] - a[key];
+                },
+                name: function(a, b) {
+                    return a[key] - b[key];
+                }
+            };
+
+        return facilities.sort(sortMethods[key]);
+    },
+
+    getInitialState: function() {
+        var sort = 'name';
+
+        return {
+            sort: sort,
+            facilities: this.getFacilitiesSortedBy(sort)
+        };
+    },
+
+    sortFacilities: function(event) {
+        var value = event.target.value;
+        this.setState({ sort: value });
+    },
+
     render: function() {
-        var facilityStats = this.getVisitedFacilities(this.props.log);
+        var facilities = this.getFacilitiesSortedBy(this.state.sort);
 
         return (
-            <table className="stats-table">
-                {facilityStats.map(function(facility) {
-                    return (
-                        <tr>
-                            <td className="stats-table__title">{facility.name}</td>
-                            <td className="stats-table__value">{facility.visits}</td>
-                        </tr>
-                    )
-                })}
-            </table>
+            <div>
+                <select onChange={this.sortFacilities} className="dropdown">
+                    <option value="name">By name</option>
+                    <option value="visits">By count</option>
+                </select>
+                <table className="stats-table">
+                    {facilities.map(function(facility) {
+                        return (
+                            <tr>
+                                <td className="stats-table__title">{facility.name}</td>
+                                <td className="stats-table__value">{facility.visits}</td>
+                            </tr>
+                        )
+                    })}
+                </table>
+            </div>
         );
     }
 });
