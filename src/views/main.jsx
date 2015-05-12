65d18b6a732f6e71c9a912c903ade629a4af750d
Erik Hedberg <erik.hedberg@klarna.com>
2015-05-12 17:18:13 +0200

    Add method for filtering number of days

--- src/views/main.jsx
+++ src/views/main.jsx
@@ -2,9 +2,11 @@ var React = require('react'),
     Reflux = require('reflux'),
     statsStore = require('../stores/stats-store.js'),
     actions = require('../actions/actions.js'),
+    moment = require('moment'),
     NumericStatsView = require('./numeric-stats.jsx'),
     WorkoutsPerDayView = require('./workouts-per-day.jsx'),
     WorkoutsPerFacilityView = require('./workouts-per-facility.jsx'),
+    DayFilterView = require('./day-filter.jsx'),
     Main;
 
 Main = React.createClass({
@@ -18,21 +20,44 @@ Main = React.createClass({
     },
 
     getInitialState: function() {
+        var log = statsStore.getCached() || [];
+
         return {
-            log: statsStore.getCached() || []
+            filteredLog: log,
+            log: log
         };
     },
 
     onUpdate: function(log) {
-        this.setState({log: log});
+        this.setState({
+            filteredLog: log,
+            log: log
+        });
+    },
+
+    setInterval: function(numDaysBack) {
+        var log = this.state.log,
+            pastDate = moment().subtract(numDaysBack, 'days'),
+            filteredLog;
+
+        filteredLog = log.reduce(function(filteredDays, curr) {
+            if (curr.date.getTime()/1E3 >= pastDate.unix()) {
+                filteredDays.push(curr);
+            }
+            return filteredDays;
+        }, []);
+
+        console.log(filteredLog.length, log.length);
+        this.setState({filteredLog: filteredLog});
     },
 
     render: function() {
         return (
             <div className="site-wrapper">
-                <NumericStatsView log={this.state.log} />
-                <WorkoutsPerDayView log={this.state.log} />
-                <WorkoutsPerFacilityView log={this.state.log} />
+                <DayFilterView onFilter={this.setInterval} />
+                <NumericStatsView log={this.state.filteredLog} />
+                <WorkoutsPerDayView log={this.state.filteredLog} />
+                <WorkoutsPerFacilityView log={this.state.filteredLog} />
             </div>
         );
     }
